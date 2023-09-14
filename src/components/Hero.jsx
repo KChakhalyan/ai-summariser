81e426e46cca75a271696a73c41a637edb2b7dba
KChakhalyan <karenchakhalyan23@gmail.com>
2023-09-14 23:27:09 +0400

    edited Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,19 @@
-import { assets } from "../assets";
+import { logo } from "../assets";
 
 const Hero = () => {
-   return <header className="w-full flex justify-center items-center flex-col"></header>;
+   return (
+      <header className="w-full flex">
+         <nav className="flex justify-between items-center w-full mb-10">
+            <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+            <button
+               className="btn"
+               type="buttonon"
+               onClick={() => window.open("https://github.com/KChakhalyan/ai-summariser")}
+            >
+               Github
+            </button>
+         </nav>
+      </header>
+   );
 };
 export default Hero;

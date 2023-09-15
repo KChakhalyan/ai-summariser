02f3dc5ea0f6bd9cf1bf3c6acc77c58fdecdadfc
KChakhalyan <karenchakhalyan23@gmail.com>
2023-09-15 15:14:20 +0400

    sixedsome issues

--- src/services/store.js
+++ src/services/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import { articleApi } from "./article";
 
 export const store = configureStore({
-   reducer: {
-      [articleApi.reducerPath]: articleApi.reducer,
-   },
-   middleware: (getDefaultMiddleWare) => getDefaultMiddleWare().concat(articleApi.middleware),
-});
+    reducer: {
+        [articleApi.reducerPath]: articleApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
+})
